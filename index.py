88a863b0bf3c31d146ffd8b3021a2c9f56a77390
congminh0502 <minhvangdanh09@gmail.com>
2025-02-10 16:47:34 +0700

    index.py

--- index.py
+++ index.py
@@ -137,7 +137,7 @@ while True:
 	elif chon == '1.2':
 		exec(requests.get('https://raw.githubusercontent.com/huongdev6868/HuongDev/refs/heads/main/Full%20Golike/AutoTikTokv2.py').text)
 	elif chon == '1.3':
-		exec(requests.get('https://raw.githubusercontent.com/huongdev6868/HuongDev/refs/heads/main/Full%20Golike/AutoIG.py').text)
+		exec(requests.get('https://raw.githubusercontent.com/congminh0502/CongMinh/refs/heads/main/toolgl/autoIG.py').text)
 
 	elif chon == '1.4':
 		exec(requests.get('https://raw.githubusercontent.com/huongdev6868/HuongDev/refs/heads/main/Full%20Golike/AutoX.py').text)
