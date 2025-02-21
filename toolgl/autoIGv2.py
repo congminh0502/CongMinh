60e52bfef82eebf3b7ccc7fea038ebe583585c01
congminh0502 <minhvangdanh09@gmail.com>
2025-02-21 20:49:31 +0700

    Update autoIGv2.py

--- toolgl/autoIGv2.py
+++ toolgl/autoIGv2.py
@@ -1,12 +1,9 @@
 import json
-import requests,os,time
-import socket
-from time import strftime
-from time import sleep
+import requests, os, time, socket, sys
+from time import strftime, sleep
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
-import time
-import sys
+
 banner = """
 \033[1;33m███    ███╗      ████████╗ █████╗  █████╗ ██╗
 \033[1;35m████  ████║      ╚══██╔══╝██╔══██╗██╔══██╗██║
@@ -18,203 +15,188 @@ banner = """
 \033[97m════════════════════════════════════════════════  
 \033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Tool\033[1;31m     : \033[1;97m☞ \033[1;31mGolike - Instagram\033[1;33m♔ \033[1;97m🔫
 \033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Youtube\033[1;31m  : \033[1;97m☞ \033[1;36mMinh Hà công\033[1;31m♔ \033[1;97m☜
-\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Tik Tok\033[1;31m  : \033[1;33mhttps:\033[1;32m//www.tiktok.com\033[1;31m/@mjng_ha
+\033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Tik Tok\033[1;31m  : \033[1;33mhttps://www.tiktok.com\033[1;31m/@mjng_ha
 \033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;97m☞\033[1;32mhttps://www.facebook.com/ha.cong.minh.556853🔫\033[1;97m☜
 \033[1;97m[\033[1;91m❣\033[1;97m]\033[1;97m Telegram\033[1;31m : \033[1;97m☞\033[1;32mhttps://t.me/minh_ha0502🔫\033[1;97m☜
 \033[97m════════════════════════════════════════════════
 """
-os.system('cls' if os.name== 'nt' else 'clear')
+
+os.system('cls' if os.name == 'nt' else 'clear')
 for x in banner:
-  print(x,end = "")
-  sleep(0.001)
-print("\033[1;31mYouTube : \033[1;33mHuong \033[1;33mDev\033[1;32m")   
+    print(x, end="")
+    sleep(0.001)
+print("\033[1;31mYouTube : \033[1;33mCông \033[1;33mMinh\033[1;32m")   
 
-    # Nhập auth
+# Nhập Authorization và Token
 try:
-  
-  Authorization = open("Authorization.txt","x")
-  t = open("token.txt","x")
+    Authorization = open("Authorization.txt", "x")
+    t = open("token.txt", "x")
 except:
-  pass
-Authorization = open("Authorization.txt","r")
-t = open("token.txt","r")
-author = Authorization.read()
-token = t.read()
+    pass
+Authorization = open("Authorization.txt", "r")
+t = open("token.txt", "r")
+author = Authorization.read().strip()
+token = t.read().strip()
 if author == "":
-  author = input("\033[1;97mNHẬP AUTHORIZATION : ")
-  token = input("\033[1;31mNHẬP T : ")
-  Authorization = open("Authorization.txt","w")
-  t = open("token.txt","w")
-  Authorization.write(author)
-  t.write(token)
+    author = input("\033[1;97mNHẬP AUTHORIZATION: ")
+    token = input("\033[1;31mNHẬP T: ")
+    with open("Authorization.txt", "w") as Authorization, open("token.txt", "w") as t:
+        Authorization.write(author)
+        t.write(token)
 else:
-  select = input("\033[1;97m║ Đăng\033[1;96m Nhập \033[1;95mTài \033[1;94mKhoản \033[1;93mHiện \033[1;92mCó\033[1;91m ( Enter Để Bỏ Qua ,Nhập AUTHORIZATION Tại Đây \033[1;97m║\033[1;91m Để Đổi )  \n\033[1;97m╚⟩⟩⟩ ")
-
-  if select != "":
-    author = select
-    token = input("\033[1;36mNhập T : ")
-    Authorization = open("Authorization.txt","w")
-    t = open("token.txt","w")
-    Authorization.write(author)
-    t.write(token)
+    select = input("\033[1;97m║ Đăng Nhập Tài Khoản Hiện Có (Enter để bỏ qua, nhập AUTHORIZATION để đổi): \n\033[1;97m╚⟩⟩⟩ ")
+    if select != "":
+        author = select
+        token = input("\033[1;36mNhập T: ")
+        with open("Authorization.txt", "w") as Authorization, open("token.txt", "w") as t:
+            Authorization.write(author)
+            t.write(token)
 Authorization.close()
 t.close()
+
 headers = {
     'Accept': 'application/json, text/plain, */*',
     'Content-Type': 'application/json;charset=utf-8',
     'Authorization': author,
     't': token,
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
     'Referer': 'https://app.golike.net/account/manager/instagram',
 }
 
-
+# Hàm lấy danh sách tài khoản Instagram
 def chonacc():
-  json_data = {}
+    response = requests.get('https://gateway.golike.net/api/instagram-account', headers=headers).json()
+    return response
 
-  response = requests.get('https://gateway.golike.net/api/instagram-account', headers=headers, json=json_data).json()
-  return response
+# Hàm lấy nhiệm vụ Follow
 def nhannv(account_id):
+    params = {
+        'account_id': account_id,
+        'data': 'null',
+    }
+    response = requests.get('https://gateway.golike.net/api/advertising/publishers/instagram/jobs',
+                            params=params, headers=headers).json()
+    return response
 
-  params = {
-    'account_id': account_id,
-    'data': 'null',
-  }
+# Hàm xác nhận hoàn thành nhiệm vụ
+def hoanthanh(ads_id, account_id):
+    json_data = {
+        'ads_id': ads_id,
+        'account_id': account_id,
+        'async': True,
+        'data': None,
+    }
+    response = requests.post('https://gateway.golike.net/api/advertising/publishers/instagram/complete-jobs',
+                             headers=headers, json=json_data).json()
+    return response
 
-  json_data = {}
+# Hàm báo lỗi và bỏ qua nhiệm vụ
+def baoloi(ads_id, object_id, account_id, loai):
+    json_data1 = {
+        'description': 'Tôi đã làm Job này rồi',
+        'users_advertising_id': ads_id,
+        'type': 'ads',
+        'provider': 'instagram',
+        'fb_id': account_id,
+        'error_type': 6,
+    }
+    try:
+        requests.post('https://gateway.golike.net/api/report/send', headers=headers, json=json_data1).json()
+    except:
+        pass
+    json_data = {
+        'ads_id': ads_id,
+        'object_id': object_id,
+        'account_id': account_id,
+        'type': loai,
+    }
+    try:
+        requests.post('https://gateway.golike.net/api/advertising/publishers/instagram/skip-jobs',
+                      headers=headers, json=json_data).json()
+    except:
+        pass
 
-  response = requests.get('https://gateway.golike.net/api/advertising/publishers/instagram/jobs',params=params,headers=headers,json=json_data,).json()
-  return response
-def hoanthanh(ads_id,account_id):
-  json_data = {
-    'ads_id': ads_id,
-    'account_id': account_id,
-    'async': True,
-    'data': None,
-  }
-
-  response = requests.post(
-    'https://gateway.golike.net/api/advertising/publishers/instagram/complete-jobs',
-    headers=headers,
-    json=json_data,
-  ).json()
-  return response
-def baoloi(ads_id,object_id,account_id,loai):
-  json_data1 = {
-    'description': 'Tôi đã làm Job này rồi',
-    'users_advertising_id': ads_id,
-    'type': 'ads',
-    'provider': 'tiktok',
-    'fb_id': account_id,
-    'error_type': 6,
-  }
-
-  response = requests.post('https://gateway.golike.net/api/report/send', headers=headers, json=json_data1).json()
-
-  json_data = {
-    'ads_id': ads_id,
-    'object_id': object_id,
-    'account_id': account_id,
-    'type': loai,
-  }
-
-  response = requests.post(
-    'https://gateway.golike.net/api/advertising/publishers/instagram/skip-jobs',
-    headers=headers,
-    json=json_data,
-  ).json()  
-
-chontkinstagram = chonacc()  
+# In danh sách tài khoản để người dùng chọn
 def dsacc():
-  if chontkinstagram.get("status") != 200:
-    print("\033[1;34mAuthorization hoặc T sai hãy nhập lại!!!")
-    quit()
+    chontkinstagram = chonacc()
+    if chontkinstagram.get("status") != 200:
+        print("\033[1;34mAuthorization hoặc Token sai, hãy nhập lại!!!")
+        quit()
+    for i, acc in enumerate(chontkinstagram.get("data", [])):
+        nickname = acc.get("nickname", "Không xác định")
+        print(f'\033[1;36m[{i+1}] \033[1;36m✈ \033[1;97mTài Khoản┊\033[1;32m㊪ :\033[1;93m {nickname} \033[1;97m|\033[1;31m㊪ :\033[1;32m Hoạt Động')
+    return chontkinstagram
 
-  for i, acc in enumerate(chontkinstagram.get("data", [])):
-    nickname = acc.get("nickname", "Không xác định")  # Nếu không có "nickname", gán giá trị mặc định
+chontkinstagram = dsacc()
+while True:
+    try:
+        luachon = int(input("\033[1;35m║ Chọn Tài Khoản Để Chạy: \n\033[1;97m╚⟩⟩⟩ "))
+        while luachon > len(chontkinstagram["data"]):
+            luachon = int(input("\033[1;32mAcc Này Không Có Trong Danh Sách, Hãy Nhập Lại: "))
+        account_id = chontkinstagram["data"][luachon - 1]["id"]
+        break
+    except:
+        print("\033[1;35mSai Định Dạng !!!")
 
-    # print(f'\033[1;97m•[✩]➭\033[1;36m [{i+1}] \033[1;91m=> \033[1;97mTên Tài Khoản┊\033[1;32m㊪ :\033[1;93m {chontktiktok["data"][i]["nickname"]}  ')
-    print(f'\033[1;36m[{i+1}] \033[1;36m✈ \033[1;97mTài Khoản┊\033[1;32m㊪ :\033[1;93m {chontkinstagram["data"][i]["nickname"]} \033[1;97m|\033[1;31m㊪ :\033[1;32m Hoạt Động')
-dsacc() 
 while True:
-  try:
-    luachon = int(input("\033[1;35m\033[1;97m║ Chọn \033[1;96mTài \033[1;95mKhoản \033[1;94mĐể \033[1;93mChạy \n\033[1;97m╚⟩⟩⟩ "))
-    while luachon > len((chontkinstagram)["data"]):
-      luachon = int(input("\033[1;32mAcc Này Không Có Trong Danh Sách , Hãy Nhập Lại : "))
-    account_id = chontkinstagram["data"][luachon - 1]["id"]
-    break  
-  except:
-    print("\033[1;35mSai Định Dạng !!!") 
+    try:
+        delay = int(input("\033[1;97m║ Nhập Delay: \n\033[1;97m╚⟩⟩⟩ "))
+        break
+    except:
+        print("\033[1;31mSai Định Dạng !!!")
+
 while True:
-  try:
-    delay = int(input("\033[1;97m║ Nhập\033[1;91m Delay \n\033[1;97m╚⟩⟩⟩ "))
-    break
-  except:
-    print("\033[1;31mSai Định Dạng !!!")
-while True:
-  try: 
-    doiacc = int(input("\033[1;97m║ \033[1;99mNhận\033[1;91m Tiền\033[1;96m Thất\033[1;95m Bại\033[1;94m Bao\033[1;93m Nhiu\033[1;92m Lần\033[1;91m Thì\033[1;96m Dừng\033[1;93m \n\033[1;97m╚⟩⟩⟩ "))
-    break
-  except:
-    print("\033[1;31mNhập Vào 1 Số!!!")    
-os.system('cls' if os.name== 'nt' else 'clear')    
+    try:
+        doiacc = int(input("\033[1;97m║ Nhận Tiền Thất Bại Bao Nhiêu Lần Thì Dừng: \n\033[1;97m╚⟩⟩⟩ "))
+        break
+    except:
+        print("\033[1;31mNhập Vào 1 Số !!!")
+
+os.system('cls' if os.name == 'nt' else 'clear')
 dem = 0
 tong = 0
 checkdoiacc = 0
 dsaccloi = []
-accloi = ""
-os.system('cls' if os.name== 'nt' else 'clear')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 for x in banner:
-  print(x,end = "")
-  sleep(0.001)
+    print(x, end="")
+    sleep(0.001)
 print(f'\033[1;36m|STT\033[1;97m| \033[1;33mThời gian ┊ \033[1;32mStatus | \033[1;31mType Job | \033[1;32mID Acc | \033[1;32mXu |\033[1;33m Tổng')
 
 while True:
-  if checkdoiacc == doiacc:
-    dsaccloi.append(chontkinstagram["data"][luachon - 1]["nickname"])
-    print(f"\033[1;36mCác Acc Instagram {dsaccloi} Có Vẻ Gặp Vấn Đề Nên Đổi Acc Chạy Đê ")
-    dsacc()
+    if checkdoiacc == doiacc:
+        dsaccloi.append(chontkinstagram["data"][luachon - 1]["nickname"])
+        print(f"\033[1;36mCác Acc Instagram {dsaccloi} Có Vẻ Gặp Vấn Đề Nên Đổi Acc Chạy Đê")
+        chontkinstagram = dsacc()
+        while True:
+            try:
+                luachon = int(input("\033[1;35m║ Chọn Tài Khoản Để Chạy: \n\033[1;97m╚⟩⟩⟩ "))
+                while luachon > len(chontkinstagram["data"]):
+                    luachon = int(input("\033[1;32mAcc Này Không Có Trong Danh Sách, Hãy Nhập Lại: "))
+                account_id = chontkinstagram["data"][luachon - 1]["id"]
+                checkdoiacc = 0
+                break  
+            except:
+                print("\033[1;35mSai Định Dạng !!!")
+    
+    print(f'\033[1;97mĐang \033[1;96mLấy \033[1;95mNhiệm \033[1;91mVụ\033[1;93m Follow', end="\r")
     while True:
-      try:
-        luachon = int(input("\033[1;35m\033[1;97m║ Chọn \033[1;96mTài \033[1;95mKhoản \033[1;94mĐể \033[1;93mChạy \n\033[1;97m╚⟩⟩⟩  "))
-        while luachon > len((chontkinstagram)["data"]):
-          luachon = int(input("\033[1;32mAcc Này Không Có Trong Danh Sách, Hãy Nhập Lại : "))
-        account_id = chontkinstagram["data"][luachon - 1]["id"]
-        checkdoiacc = 0
-        break  
-      except:
-        print("\033[1;35mSai Định Dạng !!!")
-
-     
-  print(f'\033[1;97mĐang \033[1;96mLấy \033[1;95mNhiệm \033[1;91mVụ\033[1;93m Follow',end="\r")    
-  while True:
-      try:  
-          nhanjob = nhannv(account_id)
-          break
-      except:
-          time.sleep(1)  # Thêm thời gian chờ 1 giây trước khi thử lại
-          pass
-  # while True:
-  #   try:
-  #       nhanjob = nhannv(account_id)
-  #       if nhanjob:  # Kiểm tra nếu nhanjob tồn tại và không rỗng
-  #           break  # Thoát khỏi vòng lặp nếu nhận được nhiệm vụ thành công
-  #       else:
-  #           print("\033[1;31mHệ thống đang tính toán jobs dành cho bạn,bấm load jobs lại sau 10 giây !")
-  #   except:
-  #       print("\033[1;31mHệ thống đang tính toán jobs dành cho bạn,bấm load jobs lại sau 10 giây !")
-  #       pass
-  #   time.sleep(1)
-  if nhanjob.get("status") == 200:
-    ads_id = nhanjob["data"]["id"]
-    link = nhanjob["data"]["link"]
-    object_id = nhanjob["data"]["object_id"]
-    if(nhanjob["data"]["type"] != "follow"):
-      baoloi(ads_id,object_id,account_id,nhanjob["data"]["type"])
-      continue
-    os.system(f"termux-open-url {link}")
-    for remaining_time in range(delay, -1, -1):
+        try:  
+            nhanjob = nhannv(account_id)
+            break
+        except:
+            time.sleep(1)
+            pass
+    if nhanjob.get("status") == 200:
+        ads_id = nhanjob["data"]["id"]
+        link = nhanjob["data"]["link"]
+        object_id = nhanjob["data"]["object_id"]
+        if nhanjob["data"]["type"] != "follow":
+            baoloi(ads_id, object_id, account_id, nhanjob["data"]["type"])
+            continue
+        os.system(f"termux-open-url {link}")
+        for remaining_time in range(delay, -1, -1):
             colors = [
                 "\033[1;37mM\033[1;36mi\033[1;32mn\033[1;31mh \033[1;34mD\033[1;33me\033[1;36mv\033[1;36m🍉 - Tool\033[1;36m Vip \033[1;31m\033[1;32m",
                 "\033[1;34mM\033[1;31mi\033[1;36mn\033[1;32mh \033[1;35mD\033[1;37me\033[1;33mv\033[1;32m🍉 - Tool\033[1;34m Vip \033[1;31m\033[1;32m",
@@ -227,65 +209,47 @@ while True:
             for color in colors:
                 print(f"\r{color}|{remaining_time}| \033[1;31m", end="")
                 time.sleep(0.12)
-                        
-                        
-    print("\r                          \r", end="") 
-    print("\033[1;35mĐang Nhận Tiền         ",end = "\r")
-# Vòng lặp cố gắng nhận tiền với tối đa 2 lần thử
-    max_attempts = 2
-    attempts = 0
-    nhantien = None
-
-    while attempts < max_attempts:
-        try:
-            nhantien = hoanthanh(ads_id, account_id)
-            if nhantien.get("status") == 200:  # Nhận tiền thành công
-                break
-        except:
-            pass  # Bỏ qua ngoại lệ và thử lại nếu có
-
-        attempts += 1  # Tăng số lần thử
-
-    # Kiểm tra kết quả của việc nhận tiền
-    if nhantien and nhantien.get("status") == 200:
-        dem += 1
-        tien = nhantien["data"]["prices"]
-        tong += tien
-        local_time = time.localtime()
-        hour = local_time.tm_hour
-        minute = local_time.tm_min
-        second = local_time.tm_sec
-        h = hour
-        m = minute
-        s = second
-        if hour < 10:
-            h = "0" + str(hour)
-        if minute < 10:
-            m = "0" + str(minute)
-        if second < 10:
-            s = "0" + str(second)
-
-        chuoi = (f"\033[1;31m| \033[1;36m{dem}\033[1;31m\033[1;97m | "
-                f"\033[1;33m{h}:{m}:{s}\033[1;31m\033[1;97m | "
-                f"\033[1;32msuccess\033[1;31m\033[1;97m | "
-                f"\033[1;31m{nhantien['data']['type']}\033[1;31m\033[1;32m\033[1;32m\033[1;97m |"
-                f"\033[1;32m Ẩn ID\033[1;97m |\033[1;97m \033[1;32m+{tien} \033[1;97m| "
-                f"\033[1;33m{tong}")
-
-        print("                                                    ", end="\r")
-        print(chuoi)
-        checkdoiacc = 0
-    else:
-        # Nếu cả 2 lần thử đều thất bại, bỏ qua nhiệm vụ
-        while True:
+        print("\r                          \r", end="") 
+        print("\033[1;35mĐang Nhận Tiền         ", end="\r")
+        max_attempts = 2
+        attempts = 0
+        nhantien = None
+        while attempts < max_attempts:
             try:
-                baoloi(ads_id, object_id, account_id, nhanjob["data"]["type"])
-                print("                                              ", end="\r")
-                print("\033[1;31mBỎ QUA NHIỆM VỤ ", end="\r")
-                sleep(1)
-                checkdoiacc += 1
-                break
+                nhantien = hoanthanh(ads_id, account_id)
+                if nhantien.get("status") == 200:
+                    break
             except:
-                qua = 0
                 pass
-
+            attempts += 1
+        if nhantien and nhantien.get("status") == 200:
+            dem += 1
+            tien = nhantien["data"]["prices"]
+            tong += tien
+            local_time = time.localtime()
+            hour = local_time.tm_hour
+            minute = local_time.tm_min
+            second = local_time.tm_sec
+            h = f"0{hour}" if hour < 10 else hour
+            m = f"0{minute}" if minute < 10 else minute
+            s = f"0{second}" if second < 10 else second
+            chuoi = (f"\033[1;31m| \033[1;36m{dem}\033[1;31m | "
+                     f"\033[1;33m{h}:{m}:{s}\033[1;31m | "
+                     f"\033[1;32msuccess\033[1;31m | "
+                     f"\033[1;31m{nhantien['data']['type']}\033[1;32m |"
+                     f"\033[1;32m Ẩn ID\033[1;97m |\033[1;32m +{tien} \033[1;97m| "
+                     f"\033[1;33m{tong}")
+            print("                                                    ", end="\r")
+            print(chuoi)
+            checkdoiacc = 0
+        else:
+            while True:
+                try:
+                    baoloi(ads_id, object_id, account_id, nhanjob["data"]["type"])
+                    print("                                              ", end="\r")
+                    print("\033[1;31mBỎ QUA NHIỆM VỤ ", end="\r")
+                    sleep(1)
+                    checkdoiacc += 1
+                    break
+                except:
+                    pass
